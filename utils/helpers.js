51a0c7105036748517ee948a85f8d6faa410be37
B.A. Huisken <brice.huisken@gmail.com>
2021-04-06 18:38:47 -0600

    Refactored homepage, and posts

---
+++ utils/helpers.js
@@ -0,0 +1,22 @@
+module.exports = {
+  format_date: (date) => {
+    // Format date as MM/DD/YYYY
+    return date.toLocaleDateString();
+  },
+  format_amount: (amount) => {
+    // format large numbers with commas
+    return parseInt(amount).toLocaleString();
+  },
+  get_borderBg: () => {
+    const randomNum = Math.random();
+    let borderBg = "primary";
+
+    if (randomNum > 0.7) {
+      borderBg = "secondary";
+    } else if (randomNum > 0.4) {
+      borderBg = "info";
+    }
+    return `<div class="card border-${borderBg} mb-3">
+    <div class="card-header text-white bg-${borderBg}">`
+  },
+};
