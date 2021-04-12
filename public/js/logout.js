31a1b9e2ee4834691a860aeaf2c36608f14d7729
B.A. Huisken <brice.huisken@gmail.com>
2021-04-11 23:39:11 -0600

    Set timeout for idle logged user to 5 mins. Fixed console error for user viewing own post

--- public/js/logout.js
+++ public/js/logout.js
@@ -1,5 +1,5 @@
 const logout = async () => {
-  console.log('clicked');
+  console.log('You are now logged out.');
   const response = await fetch('/api/users/logout', {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
@@ -11,5 +11,24 @@ const logout = async () => {
     alert(response.statusText);
   }
 };
+
+const inactivityTime = () => {
+  window.onload = timerReset;
+  document.onload = timerReset;
+  document.onmousemove = timerReset;
+  document.onmousedown = timerReset; // touchscreen presses
+  document.ontouchstart = timerReset;
+  document.onclick = timerReset; // touchpad clicks
+  document.onkeypress = timerReset;
+  document.addEventListener('scroll', timerReset, true); // improved; see comments
+};
+
+const timerReset = () => {
+  let time;
+  clearTimeout(time);
+  time = setTimeout(logout, 300000);
+};
+
+inactivityTime();
 
 document.querySelector('#logout').addEventListener('click', logout);
