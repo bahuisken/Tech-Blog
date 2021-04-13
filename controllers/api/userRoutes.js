7e36e1c22c9a6327ef4b490fade9def88f9435fb
B.A. Huisken <brice.huisken@gmail.com>
2021-04-12 19:18:16 -0600

    timeout changes again

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -49,13 +49,20 @@ router.post('/login', async (req, res) => {
 });
 
 router.post('/logout', (req, res) => {
-  if (req.session.logged_in) {
+  try {
     req.session.destroy(() => {
       res.status(204).end();
     });
-  } else {
+  } catch (err) {
     res.status(404).end();
   }
+  // if (req.session.logged_in) {
+  //   req.session.destroy(() => {
+  //     res.status(204).end();
+  //   });
+  // } else {
+  //   res.status(404).end();
+  // }
 });
 
 module.exports = router;
--- public/js/logout.js
+++ public/js/logout.js
@@ -1,29 +1,42 @@
 const logout = async () => {
   console.log('You are now logged out.');
-  const response = await fetch('/api/users/logout', {
+  const response = await fetch(`/api/users/logout`, {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
   });
-
+  console.log(response);
   if (response.ok) {
     document.location.replace('/');
   } else {
-    alert(response.statusText);
+    console.log(response.statusText);
   }
 };
 
-const inactivityTime = () => {
-  window.onload = timerReset;
-  document.onmousemove = timerReset;
-  document.onkeypress = timerReset;
-};
+// const inactivityTime = () => {
+//   window.onload = timerReset;
+//   document.onmousemove = timerReset;
+//   document.onkeypress = timerReset;
+// };
 
-const timerReset = () => {
+// const timerReset = () => {
+//   let time = 0;
+//   clearTimeout(time);
+//   time = setTimeout(logout, 15000);
+// };
+
+// inactivityTime();
+
+let inactivityTime = function () {
   let time;
-  clearTimeout(time);
-  time = setTimeout(logout, 300000);
-};
+  window.onload = resetTimer;
+  document.onmousemove = resetTimer;
+  document.onkeypress = resetTimer;
 
+  function resetTimer() {
+    clearTimeout(time);
+    time = setTimeout(logout, 10000);
+  }
+};
 inactivityTime();
 
 document.querySelector('#logout').addEventListener('click', logout);
